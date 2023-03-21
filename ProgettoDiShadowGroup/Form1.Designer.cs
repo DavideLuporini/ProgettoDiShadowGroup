b13362f2b17483cf712ae3d5bd1082cc590e8877
davide.luporini <davide.luporini@WINAPTMDO3D7THZ>
2023-03-21 11:04:37 +0100

    First design

--- ProgettoDiShadowGroup/Form1.Designer.cs
+++ ProgettoDiShadowGroup/Form1.Designer.cs
@@ -28,13 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox_Filter_Category = new System.Windows.Forms.ComboBox();
+            this.comboBox_Filter_Brand = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_Save_Changes = new System.Windows.Forms.Button();
+            this.btn_Filter = new System.Windows.Forms.Button();
+            this.btn_Add_Category = new System.Windows.Forms.Button();
+            this.btn_Add_Brand = new System.Windows.Forms.Button();
+            this.btn_Add_Product = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox_Filter_Category
+            // 
+            this.comboBox_Filter_Category.FormattingEnabled = true;
+            this.comboBox_Filter_Category.Location = new System.Drawing.Point(31, 147);
+            this.comboBox_Filter_Category.Name = "comboBox_Filter_Category";
+            this.comboBox_Filter_Category.Size = new System.Drawing.Size(179, 28);
+            this.comboBox_Filter_Category.TabIndex = 0;
+            // 
+            // comboBox_Filter_Brand
+            // 
+            this.comboBox_Filter_Brand.FormattingEnabled = true;
+            this.comboBox_Filter_Brand.Location = new System.Drawing.Point(235, 147);
+            this.comboBox_Filter_Brand.Name = "comboBox_Filter_Brand";
+            this.comboBox_Filter_Brand.Size = new System.Drawing.Size(179, 28);
+            this.comboBox_Filter_Brand.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 181);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(733, 216);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btn_Save_Changes
+            // 
+            this.btn_Save_Changes.Location = new System.Drawing.Point(574, 403);
+            this.btn_Save_Changes.Name = "btn_Save_Changes";
+            this.btn_Save_Changes.Size = new System.Drawing.Size(190, 35);
+            this.btn_Save_Changes.TabIndex = 3;
+            this.btn_Save_Changes.Text = "Salva";
+            this.btn_Save_Changes.UseVisualStyleBackColor = true;
+            // 
+            // btn_Filter
+            // 
+            this.btn_Filter.Location = new System.Drawing.Point(667, 147);
+            this.btn_Filter.Name = "btn_Filter";
+            this.btn_Filter.Size = new System.Drawing.Size(97, 28);
+            this.btn_Filter.TabIndex = 4;
+            this.btn_Filter.Text = "Filtra";
+            this.btn_Filter.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add_Category
+            // 
+            this.btn_Add_Category.Location = new System.Drawing.Point(31, 12);
+            this.btn_Add_Category.Name = "btn_Add_Category";
+            this.btn_Add_Category.Size = new System.Drawing.Size(202, 81);
+            this.btn_Add_Category.TabIndex = 5;
+            this.btn_Add_Category.Text = "Aggiungi Categoria";
+            this.btn_Add_Category.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add_Brand
+            // 
+            this.btn_Add_Brand.Location = new System.Drawing.Point(562, 12);
+            this.btn_Add_Brand.Name = "btn_Add_Brand";
+            this.btn_Add_Brand.Size = new System.Drawing.Size(202, 81);
+            this.btn_Add_Brand.TabIndex = 6;
+            this.btn_Add_Brand.Text = "Aggiungi Brand";
+            this.btn_Add_Brand.UseVisualStyleBackColor = true;
+            // 
+            // btn_Add_Product
+            // 
+            this.btn_Add_Product.Location = new System.Drawing.Point(304, 12);
+            this.btn_Add_Product.Name = "btn_Add_Product";
+            this.btn_Add_Product.Size = new System.Drawing.Size(202, 81);
+            this.btn_Add_Product.TabIndex = 7;
+            this.btn_Add_Product.Text = "Aggiungi Prodotto";
+            this.btn_Add_Product.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Add_Product);
+            this.Controls.Add(this.btn_Add_Brand);
+            this.Controls.Add(this.btn_Add_Category);
+            this.Controls.Add(this.btn_Filter);
+            this.Controls.Add(this.btn_Save_Changes);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox_Filter_Brand);
+            this.Controls.Add(this.comboBox_Filter_Category);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_Filter_Category;
+        private System.Windows.Forms.ComboBox comboBox_Filter_Brand;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_Save_Changes;
+        private System.Windows.Forms.Button btn_Filter;
+        private System.Windows.Forms.Button btn_Add_Category;
+        private System.Windows.Forms.Button btn_Add_Brand;
+        private System.Windows.Forms.Button btn_Add_Product;
     }
 }
 
