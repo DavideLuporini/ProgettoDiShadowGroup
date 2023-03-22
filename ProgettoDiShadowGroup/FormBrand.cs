9fe777784123c0b07152c64911c801bca3eae1fd
Giovanni Putzu <giovanni.putzu@avanade.com>
2023-03-22 16:32:14 +0100

    inserimento brand senza controlli

--- ProgettoDiShadowGroup/FormBrand.cs
+++ ProgettoDiShadowGroup/FormBrand.cs
@@ -26,10 +26,19 @@ namespace ProgettoDiShadowGroup
         private void AddBrand()
         {
             brand br = new brand();
-            br.brand_name = textBox_Brand_Name.Text;
-            ctx.brands.Add(br);
-            ctx.SaveChanges();
-            textBox_Brand_Name.Clear();
+            br.brand_name = textBox_Brand_Name.Text.Trim();
+            if (br.brand_name != ""  &&  br.brand_name != null )
+            {
+                ctx.brands.Add(br);
+                ctx.SaveChanges();
+                textBox_Brand_Name.Clear();
+                MessageBox.Show(" Hai aggiunto un nuovo Brand con successo!");
+            }
+            else
+            {
+                MessageBox.Show(" Non hai inserito nessun Brand");
+            }
+                
 
         }
 
